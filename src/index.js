cfacea1fbd8dc8ae39803c5782c1dad6b1d396c3
Phill Conrad <phillip.conrad@appfolio.com>
2019-07-05 09:29:05 -0700

    added inc and dec buttons and state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {register} from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appState = observable({
+  count : 0,
+  incCount: () => {
+    appState.count += 1;
+  },
+  decCount: () => {
+    appState.count -= 1;
+  }
+});
+
+const Counter = observer(props => (
+  <section>
+    {props.appState.count}
+    <div>
+      <button onClick={props.appState.incCount}>Add</button>
+      <button onClick={props.appState.decCount}>Dec</button>
+    </div>
+  </section>
+));
+
+ReactDOM.render(
+  <Counter appState={appState} />,
+  document.getElementById('root')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
